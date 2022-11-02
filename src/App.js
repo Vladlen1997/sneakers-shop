c70359e366a6e29fa63f1020b109eaa683e5991f
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-02 20:38:37 +0300

    Added components and import them

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Card from "./Components/Card/Card";
+import Header from "./Components/Header/Header";
+import Drawer from "./Components/Drawer/Drawer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="wrapper clear">
+            <Drawer/>
+
+            <Header/>
+
+
+            <div className="content p-40">
+
+                <div className='d-flex align-center justify-between mb-40'>
+                    <h1>Все кроссовки</h1>
+                    <div className="search-block d-flex align-center">
+                        <img src="/img/search.svg" alt="searcher"/>
+                        <input placeholder='Поиск ...' type="text"/>
+                    </div>
+                </div>
+
+
+                <div className='d-flex'>
+
+                    <Card/>
+                    <Card/>
+                    <Card/>
+
+
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
