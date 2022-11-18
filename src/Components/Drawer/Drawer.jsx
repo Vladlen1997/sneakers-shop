bac73b52d8dc05f9d7d63182238274e21b6bb362
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-18 20:04:27 +0300

    Created Content.module.scss & renamed classes at Drawer.jsx

--- src/Components/Drawer/Drawer.jsx
+++ src/Components/Drawer/Drawer.jsx
@@ -1,66 +1,57 @@
-import React from 'react';
+import React from "react";
+import classes from "./Drawer.module.scss";
+
 
 const Drawer = () => {
     return (
-        <div style={{display: 'none'}} className="overlay">
-        <div className="drawer-block">
-            <h2 className='mb-15 d-flex justify-between'>Корзина
-                <img className='cu-p' src="/img/btn-remove.svg" alt="remove"/>
-            </h2>
+        <div style={{display: 'none'}} className={classes.overlay}>
+            <div className={classes.drawer}>
+                <h2 className="mb-15 d-flex justify-between align-center">
+                    Корзина
+                    <img className="remove-cart cu-p" src="/img/btn-remove.svg" alt="Remove"/>
 
-            <div className="items">
+                </h2>
 
+                <div className={classes.items}>
+                    <div className="cartItem d-flex align-center mb-20">
 
-                <div className="cartItem d-flex align-center mb-20">
-                    <img className='mr-15' width={70} height={70} src="/img/sneakers/sneak8.jpg"
-                         alt="Sneakers"/>
-                    <div className='mr-30'>
-                        <p className='mb-5'>Мужские кроссовки Nike Air Max 270</p>
-                        <b>12.999 руб.</b>
+                        <div style={{backgroundImage: 'url(/img/sneakers/sneak1.jpg)'}}
+                             className="cartItemImg"></div>
+
+                        <div className="mr-20 flex">
+                            <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                            <b>12 999 руб.</b>
+                        </div>
+                        <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove"/>
+                    </div>
+                </div>
+                <div className="cartTotalBlock">
+                    <ul className="cartTotalBlock mb-25">
+                        <li className="d-flex justify-between">
+                            <span>Цена:</span>
+                            <div></div>
+                            <b>21 498 руб. </b>
+                        </li>
+                        <li className="d-flex justify-between">
+                            <span>Налог 5%:</span>
+                            <div></div>
+                            <b>1074 руб. </b>
+                        </li>
+                    </ul>
+
+                    <div className="cart-button">
+                        <button className="greenButton">
+                            Оформить заказ
+                            <img src="/img/Vector.svg" alt="Vector"/>
+                        </button>
                     </div>
 
 
-                    <img className='removeBtn' src="/img/r-btn.svg" alt="button"/>
-                </div>
-
-
-                <div className="cartItem d-flex align-center">
-                    <img className='mr-15' width={70} height={70} src="/img/sneakers/sneak8.jpg"
-                         alt="Sneakers"/>
-                    <div className='mr-30'>
-                        <p className='mb-5'>Мужские кроссовки Nike Air Max 270</p>
-                        <b>12.999 руб.</b>
-                    </div>
-
-                    <img className='removeBtn' src="/img/r-btn.svg" alt="button"/>
                 </div>
             </div>
-
-            <div className='cartTotalBlock'>
-
-                <ul>
-                    <li>
-                        <span>Итого:</span>
-                        <div></div>
-                        <b>2698 р.</b>
-                    </li>
-                    <li>
-                        <span>Налог 5%:</span>
-                        <div></div>
-                        <b>655 р.</b>
-                    </li>
-                </ul>
-
-                <button className='greenButton'>
-                    оформить заказ
-                    <img src="/img/Vector.png" alt="arrow"/>
-                </button>
-
-
-            </div>
         </div>
-        </div>
+    )
+}
 
-            )
-            }
-            export default Drawer;
+
+export default Drawer;
