b2c5fe4eb7e12c7bf05b2890bba04fa1f274676a
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2022-11-22 17:22:40 +0300

    Created useState hooks and uploaded an array with data to the server

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
-import './App.scss';
+import "./App.scss";
+import React from 'react';
 import Card from "./Components/Card/Card";
 import Header from "./Components/Header/Header";
 import Drawer from "./Components/Drawer/Drawer";
+import Content from "./Components/Content/Content";
+
 
 const App = () => {
-    return (
-        <div className="wrapper clear">
-            <Drawer/>
+    const [items, setItems] = React.useState([]);
+    const [cartItems, setCartItems] = React.useState([]);
 
-            <Header/>
+
+    React.useEffect(() => {
+
+        fetch("https://637b689d10a6f23f7fa7ea2b.mockapi.io/Items").then((res) => {
+            return res.json();
+        }).then(json => {
+            setItems(json);
+        });
+
+    }, []);
+
+
+    const [cartOpened, setCartOpened] = React.useState(false);
+
+
+    return (
+
+        <div className="wrapper clear">
+
+            {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)}/>}
+
+            <Header onCartClick={() => setCartOpened(true)}/>
+
+
+            {/*Content Page ---> to Component*/}
 
 
             <div className="content p-40">
 
-                <div className='d-flex align-center justify-between mb-40'>
-                    <h1>Все кроссовки</h1>
-                    <div className="search-block d-flex align-center">
-                        <img src="/img/search.svg" alt="searcher"/>
-                        <input placeholder='Поиск ...' type="text"/>
-                    </div>
-                </div>
+                <Content/>
 
-
-                <div className='d-flex'>
-
-                    <Card/>
-                    <Card/>
-                    <Card/>
+                <div className="sneakers d-flex flex-wrap">
+                    {items.map((obj) => <Card name={obj.name} price={obj.price} image={obj.image}/>)}
 
 
                 </div>
 
             </div>
+
+
         </div>
-    );
+    )
 }
+
 
 export default App;
